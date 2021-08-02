61adb5d34f1696dfd9f10409e171b757b30e0744
JayThibs <thibo.jacques@gmail.com>
2021-08-02 14:52:05 -0300

    Added post request and pydantic model.

--- fastapi-tutorial/src/app.py
+++ fastapi-tutorial/src/app.py
@@ -1,11 +1,19 @@
 from fastapi import FastAPI
+from pydantic_models import Item
 
 app = FastAPI()
+
 
 @app.get("/")
 def root():
     return {"message": "Hello World"}
 
+
 @app.get("/users/{user_id}")
 def read_user(user_id: int):
     return {"user_id": user_id}
+
+
+@app.post("/items/")
+def create_item(item: Item):
+    return item

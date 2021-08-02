021d4ffb8d4f4bdd9f8b39766c58180beebec708
JayThibs <thibo.jacques@gmail.com>
2021-08-02 11:57:32 -0300

    Added Makefile to run fastapi.

---
+++ fastapi-tutorial/src/app.py
@@ -0,0 +1,11 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def root():
+    return {"message": "Hello World"}
+
+@app.get("/users/{user_id}")
+def read_user(user_id: int):
+    return {"user_id": user_id}
